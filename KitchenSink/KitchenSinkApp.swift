ebe5d4cb420c5927046abb4660b92fcd3c138431
Sven Günther <inevs@me.com>
2026-01-17 12:05:45 +0100

    Initial Commit

---
+++ KitchenSink/KitchenSinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenSinkApp.swift
+//  KitchenSink
+//
+//  Created by Sven Günther on 17.01.26.
+//
+
+import SwiftUI
+
+@main
+struct KitchenSinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
